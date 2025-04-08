b7045e14d73a2084daee0711210e7afc4386c6d3
Rafael Ribeiro Estrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2025-04-07 23:22:47 -0400

    Update StorageServiceImpl.java

--- desafio-processo-seletivo-seplag-api/src/main/java/com/example/desafioprocessoseletivoseplagapi/services/impl/StorageServiceImpl.java
+++ desafio-processo-seletivo-seplag-api/src/main/java/com/example/desafioprocessoseletivoseplagapi/services/impl/StorageServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.desafioprocessoseletivoseplagapi.services.impl;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.example.desafioprocessoseletivoseplagapi.dtos.FotoDTO;
+import com.example.desafioprocessoseletivoseplagapi.providers.exceptions.LayerDefinition;
+import com.example.desafioprocessoseletivoseplagapi.providers.exceptions.StorageException;
+import com.example.desafioprocessoseletivoseplagapi.providers.exceptions.enums.LayerEnum;
 import com.example.desafioprocessoseletivoseplagapi.services.StorageService;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +21,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
-public class StorageServiceImpl implements StorageService {
+public class StorageServiceImpl implements StorageService, LayerDefinition {
 
     private final AmazonS3 s3Client;
 
@@ -27,26 +31,36 @@ public class StorageServiceImpl implements StorageService {
 
     @Override
     public void upload(FotoDTO dto, String bucket, String key) {
-        Map<String, String> map = new HashMap<>();
-        map.put("filename", sanitize(dto.getFilename()));
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentType(dto.getContentType());
-        objectMetadata.setContentLength(dto.getContent().length);
-        objectMetadata.setUserMetadata(map);
-        PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, key, new ByteArrayInputStream(dto.getContent()), objectMetadata);
-        s3Client.putObject(putObjectRequest);
+        try {
+            Map<String, String> map = new HashMap<>();
+            map.put("filename", sanitize(dto.getFilename()));
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(dto.getContentType());
+            objectMetadata.setContentLength(dto.getContent().length);
+            objectMetadata.setUserMetadata(map);
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, key, new ByteArrayInputStream(dto.getContent()), objectMetadata);
+            s3Client.putObject(putObjectRequest);
+        } catch (AmazonServiceException e) {
+            throw new StorageException("Erro ao salvar arquivo: " + e.getMessage(), this);
+        }
+
     }
 
     @Override
     public FotoDTO download(String bucket, String key) throws IOException {
-        ObjectMetadata objectMetadata = s3Client.getObject(new GetObjectRequest(bucket, key)).getObjectMetadata();
-        GetObjectRequest getObjectRequest = new GetObjectRequest(bucket, key);
-        byte[] content = s3Client.getObject(getObjectRequest).getObjectContent().readAllBytes();
-        FotoDTO dto = new FotoDTO();
-        dto.setContent(content);
-        dto.setContentType(objectMetadata.getContentType());
-        dto.setFilename(objectMetadata.getUserMetadata().get("filename"));
-        return dto;
+        try {
+            ObjectMetadata objectMetadata = s3Client.getObject(new GetObjectRequest(bucket, key)).getObjectMetadata();
+            GetObjectRequest getObjectRequest = new GetObjectRequest(bucket, key);
+            byte[] content = s3Client.getObject(getObjectRequest).getObjectContent().readAllBytes();
+            FotoDTO dto = new FotoDTO();
+            dto.setContent(content);
+            dto.setContentType(objectMetadata.getContentType());
+            dto.setFilename(objectMetadata.getUserMetadata().get("filename"));
+            return dto;
+        } catch (AmazonServiceException e) {
+            throw new StorageException("Erro ao recuperar arquivo: " + e.getMessage(), this);
+        }
+
     }
 
     @Override
@@ -64,4 +78,13 @@ public class StorageServiceImpl implements StorageService {
     }
 
 
+    @Override
+    public String getClassName() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public LayerEnum getLayer() {
+        return LayerEnum.API_COMPONENT;
+    }
 }
